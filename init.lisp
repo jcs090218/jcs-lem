e45a698514e1836836bfd38a33ea6afbc69a207e
JenChieh <jcs090218@gmail.com>
2023-06-05 13:58:49 -0700

    Add nav

--- init.lisp
+++ init.lisp
@@ -4,3 +4,10 @@
 (in-package :lem-jcs-init)
 
 (lem:load-theme "emacs-dark")
+
+;; Navigation
+(define-key *global-keymap* "M-w" 'other-window)
+(define-key *global-keymap* "M-q" 'other-window)
+
+(define-key *global-keymap* "C-Home" 'move-to-beginning-of-buffer)
+(define-key *global-keymap* "C-End" 'move-to-end-of-buffer)
