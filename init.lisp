abca0e7b180da1b47699018cfce71d51d4826653
Jen-Chieh Shen <jcs090218@gmail.com>
2024-10-09 23:44:47 +0800

    feat: Add headers

--- init.lisp
+++ init.lisp
@@ -1,3 +1,14 @@
+#| # Load the full configuration
+
+Author:  Shen, Jen-Chieh <jcs090218@gmail.com>
+URL:     https://github.com/jcs090218/jcs-lem
+
+This file bootstraps the configuration, which is divided into a number of
+other files.
+
+License: BSD-2-Clause
+|#
+
 (defpackage #:jcs
   (:use #:cl #:lem))
 
--- lisp/jcs-key.lisp
+++ lisp/jcs-key.lisp
@@ -1,3 +1,5 @@
+#| # Global Key Definition |#
+
 (defpackage :jcs-key
   (:use #:cl #:lem))
 
